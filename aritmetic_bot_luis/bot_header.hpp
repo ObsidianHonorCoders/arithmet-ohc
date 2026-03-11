981ab61272e8a69575ba368edf48393bf570cd35
Calileus <79671075+Calileus@users.noreply.github.com>
2026-03-10 22:48:17 -0600

    Adding aritmetic_bot_luis draft

---
+++ aritmetic_bot_luis/bot_header.hpp
@@ -0,0 +1,11 @@
+#ifndef LUIS_BOT_HEADER_HPP
+#define LUIS_BOT_HEADER_HPP
+
+#include <string>
+
+namespace luis_aritmetic_bot
+{
+  double calculate(std::string expression);
+}
+
+#endif // LUIS_BOT_HEADER_HPP
