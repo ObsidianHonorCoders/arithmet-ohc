dea709c26bd791ca78526ef2e387f20551f12c16
Calileus <79671075+Calileus@users.noreply.github.com>
2026-03-10 22:48:29 -0600

    Adding aritmetic_bot_marco draft

---
+++ aritmetic_bot_marco/bot_header.hpp
@@ -0,0 +1,11 @@
+#ifndef MARCO_BOT_HEADER_HPP
+#define MARCO_BOT_HEADER_HPP
+
+#include <string>
+
+namespace marco_aritmetic_bot
+{
+  double calculate(std::string expression);
+}
+
+#endif // MARCO_BOT_HEADER_HPP
