05825c1a20582f7f9e1a1da32af79ec903a88fc0
Calileus <79671075+Calileus@users.noreply.github.com>
2026-03-10 22:48:00 -0600

    Adding aritmetic_bot_calileus draft

---
+++ aritmetic_bot_calileus/bot_functions.cpp
@@ -0,0 +1,8 @@
+#include "bot_header.hpp"
+
+double calileus_aritmetic_bot::calculate(std::string expression)
+{
+  // TODO: Implement calculation logic
+
+  return 0.0;
+}
---
+++ aritmetic_bot_calileus/bot_header.hpp
@@ -0,0 +1,11 @@
+#ifndef CALILEUS_BOT_HEADER_HPP
+#define CALILEUS_BOT_HEADER_HPP
+
+#include <string>
+
+namespace calileus_aritmetic_bot
+{
+  double calculate(std::string expression);
+}
+
+#endif // CALILEUS_BOT_HEADER_HPP
